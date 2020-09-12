9e697f8c23a9c66cca707bb5a201cbb2990e8e0d
GabrielDevss <gabriel0211oliveira15@gmail.com>
2020-09-12 18:32:13 -0300

    crud-master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CandidatosComponent } from './candidatos/candidatos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
+  { path: 'cadastro', component:  CadastroComponent},
+  { path: 'candidatos', component: CandidatosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CandidatosComponent } from './candidatos/candidatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    HomeComponent,
+    CadastroComponent,
+    CandidatosComponent
   ],
   imports: [
     BrowserModule,
